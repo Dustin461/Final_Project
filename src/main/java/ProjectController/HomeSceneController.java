7cb79e58455f7d4e31f686c965dcad7936b4bf2d
Anh Pham <S3802674@student.rmit.edu.au>
2022-05-19 16:38:48 +0700

    Update HomeSceneController.java and HomeScene.fxml

--- src/main/java/ProjectController/HomeSceneController.java
+++ src/main/java/ProjectController/HomeSceneController.java
@@ -1,7 +1,16 @@
 package ProjectController;
 
+import javafx.application.Application;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressBar;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,7 +19,22 @@ public class HomeSceneController implements Initializable {
     public static ProgressBar progressBar = new ProgressBar();
     public static ProgressBar newestProgressBar = new ProgressBar();
 
+    @FXML
+    private Button closeButton;
+    @FXML
+    private Button refreshButton;
+
+
+    public void refreshButton(ActionEvent e){
+
+    }
+
+    public void closeButton(ActionEvent e){
+        Platform.exit();
+    }
+
     public HomeSceneController() {
+
         //Set the progress bar
         progressBar.setProgress(0.0);
         progressBar.setPrefHeight(5);
--- src/main/java/ProjectController/WelcomeSceneController.java
+++
@@ -1,4 +0,0 @@
-package ProjectController;
-
-public class WelcomeSceneController {
-}
