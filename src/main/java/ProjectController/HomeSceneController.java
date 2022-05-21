f8205c8e7262e8794969100b77e4c4cb1001cd19
Anh Pham <S3802674@student.rmit.edu.au>
2022-05-21 17:09:57 +0700

    Update controller

--- src/main/java/ProjectController/HomeSceneController.java
+++ src/main/java/ProjectController/HomeSceneController.java
@@ -1,40 +1,106 @@
 package ProjectController;
 
-import javafx.application.Application;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ProgressBar;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Pagination;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import ProjectArticle.Article;
+import ProjectArticle.ArticleList;
+import ProjectController.LayoutController;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.CacheHint;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeSceneController implements Initializable {
-    public static ProgressBar progressBar = new ProgressBar();
-    public static ProgressBar newestProgressBar = new ProgressBar();
+    @FXML
+    private Button BusinessButton;
+
+    @FXML
+    private Button CovidButton;
+
+    @FXML
+    private Button EntertainmentButton;
+
+    @FXML
+    private Button HealthButton;
+
+    @FXML
+    private Button NewestButton;
+
+    @FXML
+    private Button OthersButton;
+
+    @FXML
+    private Button PoliticsButton;
+
+    @FXML
+    private Button SportButton;
+
+    @FXML
+    private Button WorldButton;
+
+    @FXML
+    private BorderPane borderPaneUnderScrollPane;
 
     @FXML
     private Button closeButton;
+
     @FXML
     private Button refreshButton;
 
+    @FXML
+    private ScrollPane scrollPane;
 
-    public void refreshButton(ActionEvent e){
+    @FXML
+    private Button searchButton;
 
-    }
+    @FXML
+    private TextField searchTextField;
 
-    public void closeButton(ActionEvent e){
+    @FXML
+    private Button technologyButton;
+
+    @FXML
+    void closeButton(ActionEvent event) {
         Platform.exit();
     }
 
-    public HomeSceneController() {
+    @FXML
+    void refreshButton(ActionEvent event) {
 
+    }
+
+    public String searchText;
+    public static ProgressBar progressBar = new ProgressBar();
+    public static ProgressBar newestProgressBar = new ProgressBar();
+
+    // Pagination control
+    public int currentCategoryIndex = 0, currentArticleIndex = 0;
+    public ArrayList<Article> currentCategoryList;
+    public Pagination currentPagination;
+
+    // Instance Layout controller
+    public LayoutController layoutController;
+
+    //Vbox to display layout
+    public VBox displayLayoutVbox = new VBox();
+
+    public HomeSceneController() {
         //Set the progress bar
         progressBar.setProgress(0.0);
         progressBar.setPrefHeight(5);
@@ -42,6 +108,8 @@ public class HomeSceneController implements Initializable {
         newestProgressBar.setProgress(0.0);
         newestProgressBar.setPrefHeight(5);
         newestProgressBar.setPrefWidth(200);
+
+
     }
 
     @Override
--- src/main/java/ProjectController/LayoutController.java
+++ src/main/java/ProjectController/LayoutController.java
@@ -1,4 +1,146 @@
 package ProjectController;
 
-public class LayoutController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class LayoutController implements Initializable {
+    @FXML
+    private AnchorPane anchorpane0;
+
+    @FXML
+    private AnchorPane anchorpane1;
+
+    @FXML
+    private AnchorPane anchorpane10;
+
+    @FXML
+    private AnchorPane anchorpane2;
+
+    @FXML
+    private AnchorPane anchorpane3;
+
+    @FXML
+    private AnchorPane anchorpane4;
+
+    @FXML
+    private AnchorPane anchorpane5;
+
+    @FXML
+    private AnchorPane anchorpane6;
+
+    @FXML
+    private AnchorPane anchorpane7;
+
+    @FXML
+    private AnchorPane anchorpane8;
+
+    @FXML
+    private AnchorPane anchorpane9;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button10;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private VBox displayLayoutVbox;
+
+    @FXML
+    private Text textSource1;
+
+    @FXML
+    private Text textSource10;
+
+    @FXML
+    private Text textSource2;
+
+    @FXML
+    private Text textSource3;
+
+    @FXML
+    private Text textSource4;
+
+    @FXML
+    private Text textSource5;
+
+    @FXML
+    private Text textSource6;
+
+    @FXML
+    private Text textSource7;
+
+    @FXML
+    private Text textSource8;
+
+    @FXML
+    private Text textSource9;
+
+    @FXML
+    private Text textTitle5;
+
+    @FXML
+    private Text textTitle1;
+
+    @FXML
+    private Text textTitle10;
+
+    @FXML
+    private Text textTitle2;
+
+    @FXML
+    private Text textTitle3;
+
+    @FXML
+    private Text textTitle4;
+
+    @FXML
+    private Text textTitle6;
+
+    @FXML
+    private Text textTitle7;
+
+    @FXML
+    private Text textTitle8;
+
+    @FXML
+    private Text textTitle9;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
